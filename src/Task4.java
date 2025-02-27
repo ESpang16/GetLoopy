92c83c4314e38294f1eb83dbe35a9bdae8a215af
Evan <spanglem@uc.edu>
2025-02-27 14:25:26 -0500

    Task 4 Final Commit

---
+++ src/Task4.java
@@ -0,0 +1,7 @@
+public class Task4 {
+    public static void main(String[] args){
+        for(int counter= 10; counter>=0; counter-=2){
+            System.out.print(counter + " ");
+        }
+    }
+}
