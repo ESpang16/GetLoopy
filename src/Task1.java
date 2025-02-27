de50a7b42d546162dd10696038fe204c20c8a2f0
Evan <spanglem@uc.edu>
2025-02-27 14:16:43 -0500

    Task 1 final commit

--- src/Task1.java
+++ src/Task1.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Task1 {
     public static void main(String[] args) {
         for(int counter = 0; counter <= 30;counter++){
             System.out.print(counter + " ");
